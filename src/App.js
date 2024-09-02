35ada4415aec56694b539a262f59c05b8f756a79
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-09-02 11:41:32 -0700

    If applied, commit adds integration testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 // src/App.js
 
+import { useEffect, useState } from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumbersOfEvents";
+import { getEvents } from "./api";
 import "./App.css";
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  //NOE -> number of events
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
