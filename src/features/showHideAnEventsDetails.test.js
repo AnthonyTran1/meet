ce44e43881c9727e7a38f35d9ebe5090e0d0b1fd
AnthonyTran696 <AnthonyTran696@gmail.com>
2024-09-06 10:31:15 -0700

    If applied, commit updates showHideAnEventsDetails

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -6,6 +6,24 @@ import App from "../App";
 import userEvent from "@testing-library/user-event";
 
 const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
+const dummyEvent = {
+  kind: "calendar#event",
+  etag: '"3181161784712000"',
+  id: "4eahs9ghkhrvkld72hogu9ph3e_20200519T140000Z",
+  status: "confirmed",
+  htmlLink:
+    "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MTlUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+  created: "2020-05-19T19:17:46.000Z",
+  updated: "2020-05-27T12:01:32.356Z",
+  summary: "Learn JavaScript",
+  description:
+    "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
+  location: "London, UK",
+  creator: {
+    email: "fullstackwebdev@careerfoundry.com",
+    self: true,
+  },
+};
 
 defineFeature(feature, (test) => {
   test("An event element is collapsed by default.", ({ given, when, then }) => {
@@ -17,8 +35,8 @@ defineFeature(feature, (test) => {
     });
 
     then("the details of the events is collapsed by default", async () => {
-      const allEvents = await getEvents();
-      const EventComponent = render(<Event event={allEvents[0]} />);
+      // const allEvents = await getEvents();
+      const EventComponent = render(<Event event={dummyEvent} />);
       expect(
         EventComponent.container.querySelector(".details")
       ).not.toBeInTheDocument();
@@ -34,9 +52,9 @@ defineFeature(feature, (test) => {
     let EventComponent;
     when("user clicks on an event", async () => {
       const user = userEvent.setup();
-      const allEvents = await getEvents();
-      const AppDOM = AppComponent.container.firstChild;
-      EventComponent = render(<Event event={allEvents[0]} />);
+      // const allEvents = await getEvents();
+      // const AppDOM = AppComponent.container.firstChild;
+      EventComponent = render(<Event event={dummyEvent} />);
       await user.click(EventComponent.queryByText("show details"));
     });
 
@@ -60,9 +78,9 @@ defineFeature(feature, (test) => {
       AppComponent = render(<App />);
 
       user = userEvent.setup();
-      const allEvents = await getEvents();
-      const AppDOM = AppComponent.container.firstChild;
-      EventComponent = render(<Event event={allEvents[0]} />);
+      // const allEvents = await getEvents();
+      // const AppDOM = AppComponent.container.firstChild;
+      EventComponent = render(<Event event={dummyEvent} />);
       await user.click(EventComponent.queryByText("show details"));
       expect(
         EventComponent.container.querySelector(".details")
